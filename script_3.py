442956fe8f16406cafbd9bc13a7bd3db14843d0f
chelydra-ua <lipara.shep@gmail.com>
2022-07-21 23:46:36 +0300

    Create script_3.py

---
+++ script_3.py
@@ -0,0 +1,6 @@
+import random
+
+characters = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ&*(){}[]|/\?!@#$%^abcdefghijklmnopqrstuvwxyz"
+password_length = int(input('Enter the length of the password: '))
+password = "".join(random.sample(characters, password_length))
+print("Gernerated password: %s" % password)
